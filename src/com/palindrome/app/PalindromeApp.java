dbffe5118556d9d51c476aaef397e22020405eae
Jarvis-the-og <jarvis.rdpandey@gmail.com>
2026-02-27 14:27:28 +0530

    UC1: Add welcome message module

---
+++ src/com/palindrome/app/PalindromeApp.java
@@ -0,0 +1,14 @@
+package com.palindrome.app;
+
+public class PalindromeApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" PALINDROME CHECKER APP ");
+        System.out.println("================================");
+
+        System.out.println("Welcome to Palindrome Checker Application");
+
+    }
+}
