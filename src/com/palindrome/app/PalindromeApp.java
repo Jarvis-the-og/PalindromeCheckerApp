aca742857193adf60810fb5b6cb1862df900b461
Jarvis-the-og <jarvis.rdpandey@gmail.com>
2026-02-27 14:35:16 +0530

    UC2:Hardcoded palindrome

--- src/com/palindrome/app/PalindromeApp.java
+++ src/com/palindrome/app/PalindromeApp.java
@@ -8,7 +8,23 @@ public class PalindromeApp {
         System.out.println(" PALINDROME CHECKER APP ");
         System.out.println("================================");
 
-        System.out.println("Welcome to Palindrome Checker Application");
+        String word = "madam";
+
+        String reversed = "";
+
+        for(int i = word.length()-1; i >= 0; i--)
+        {
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed))
+        {
+            System.out.println(word + " is Palindrome");
+        }
+        else
+        {
+            System.out.println(word + " is NOT Palindrome");
+        }
 
     }
 }
